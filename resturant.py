c991d12b45d7b5d7a8a21d0e598cd602f440c8c2
youngs01 <124503790+youngs01@users.noreply.github.com>
2026-01-02 15:36:30 +0500

    Completes waiter function

--- resturant.py
+++ resturant.py
@@ -11,72 +11,58 @@ class Resturant:
     def __init__(self):
         self.order = dict({})
 
-
     def show_banner(self):
         """ SHows Banner in the terminal"""
         f = Figlet(font="rowancap")
         print(f.renderText("Welcome To Termurant"))
 
-
-
     @classmethod
     def show_menu(cls):
         """ Show menu """
+        print(f"{'='*11} Menu {'='*11}")
         print(f'{"Item":<20} {"Price":>6}')
         print("=" * 28)
 
         with open("menu.csv", "r", encoding="utf-8") as file:
             for row in csv.DictReader(file):
                 name = row["Name"]
-                cls.menu.append(name)
+                cls.menu.append(name.lower())
                 price = int(row["Price"])
                 print(f'{name:<21} $ {price:02}')
-
         print("=" * 28)
 
-
-
     def waiter(self):
-        """ Summary """
+        """ Ask User About there order """
         while True:
             try:
                 print()
-                item = input("What would you like to have: ")
+                item = input("What would you like to have? ")
                 food, quantity = self.validate_and_ditribute(item)
-                
+
                 if food in self.order:
                     self.order[food.lower()] += quantity
                 else:
                     self.order[food.lower()] = quantity
-                
-                if self.verfiy_re_order:
+
+                if self.verifier("Anything else"):
                     break
 
             except ValueError as e:
+                print()
                 print(f"Error: {str(e)}")
 
-        for key, value in self.order.items():
-            print(key,value)
-    
-    
-    @property
-    def verfiy_re_order(self):
-        print()
-        res = input("Would you like to order something else (y/n): ").strip()
+    def verifier(self,msg):
         while True:
-            try:
-                if res.lower() not in ['y','n']:
-                    raise ValueError("Please enter 'y' or 'n'")
-                
-                break
-            except ValueError as e:
-                print(f"Error: {str(e)}")
-                
-        if res.lower() == 'n':
-            return True
-                        
+            print()
+            res = input(f"{msg}? (y/n): ").strip().lower()
 
-        
+            if res == 'n':
+                return True
+            elif res == 'y':
+                return False
+            else:
+                print()
+                print("Error: Please enter 'y' or 'n'")
 
     def validate_and_ditribute(self, text):
         """Summary"""
@@ -87,7 +73,23 @@ class Resturant:
 
         food, quantity = text.lower().split('x')
 
+        if food.strip().lower() not in self.menu:
+            raise ValueError("The food is not in the menu")
+
         return (food.strip(), int(quantity.strip()))
+
+    def repeat_order(self):
+        print()
+        print(f"{'='*15} Your Order {'='*15} ")
+
+        for key, value in self.order.items():
+            print(f"{value} {key}")
+    
+    def order_confirm(self):
+        print()
+        if self.verifier("Is this your final order"):
+            self.waiter()
+    
 
 
 def main():
@@ -97,6 +99,10 @@ def main():
     print()
     print('=============== CAUTION: WRITE YOUR ORDER IN THIS SEQUENCE i-e ( burger X 2 ) =============== ')
     rest.waiter()
+    rest.repeat_order()
+    rest.order_confirm()
+    # rest.order_total()
+    print(rest.order.items())
 
 
 if __name__ == '__main__':
